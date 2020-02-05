83563956a5fbbdb279827184df632ddb35a21e14
Mantux99 <cerkesas.mantas@gmail.com>
2020-02-05 08:22:13 +0100

    antra uzduotis done

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 //pirma uzduotis, sujungti kad printintu mano batai buvo du
-$a = 'du';
-$b = 'mano';
-$c = 'batai buvo';
+// $a = 'du';
+// $b = 'mano';
+// $c = 'batai buvo';
 
-print $b.' '.$c.' '.$a;
+// print $b.' '.$c.' '.$a;
 //antra uzduotis
 
-//pakeista
+$a ='45';
+$b ='12';
+
+if ($a>$b) {
+    print 'a daugiau uz b';
+}else {
+    print 'b daugiau uz a';
+}
 
 
 
